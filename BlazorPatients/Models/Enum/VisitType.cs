8968588a7c5bc1fe5450620aa2047f7e60986864
ThePaco <pv_hr@hotmail.com>
2025-08-29 21:29:51 +0200

    moving from mvc to blazor

---
+++ BlazorPatients/Models/Enum/VisitType.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorPatients.Models.Enum;
+
+public enum VisitType
+{
+    GP,
+    KRV,
+    XRAY,
+    CT,
+    MR,
+    ULTRA,
+    EKG,
+    ECHO,
+    EYE,
+    DERM,
+    DENTA,
+    MAMMO,
+    NEURO
+}
---
+++ BlazorPatients/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorPatients.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsMale { get; set; }
+    public string Oib { get; set; }
+    public DateTime Birthday { get; set; }
+
+}
